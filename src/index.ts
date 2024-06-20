9913638c89f14fe8e2d9816b4cd1772cdb343173
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2024-06-20 16:54:24 +0200

    Add catch all

--- src/index.ts
+++ src/index.ts
@@ -9,38 +9,17 @@ import { updateStations, updateEnd, updateStationsGeo } from './updateDisruption
 
 async function checkDisruptions() {
     console.log('Checking disruptions...');
-    const disruptions: any = await apiGetDisruptions();
-    let existingDisruptions = await databaseGetDisruptions();
-    const existingIds = existingDisruptions.map(disruption => disruption.nsId);
+    try {
+        const disruptions: any = await apiGetDisruptions();
+        let existingDisruptions = await databaseGetDisruptions();
+        const existingIds = existingDisruptions.map(disruption => disruption.nsId);
 
-    disruptions.forEach(async (disrupt: any) => {
-        if (!existingIds.includes(disrupt.id)) {
-            createDisruption(disrupt.id, disrupt.timespans[0].cause.label, new Date(disrupt.timespans[0].start), disrupt.titleSections, disrupt);
-            existingDisruptions = await databaseGetDisruptions();
+        disruptions.forEach(async (disrupt: any) => {
+            if (!existingIds.includes(disrupt.id)) {
+                createDisruption(disrupt.id, disrupt.timespans[0].cause.label, new Date(disrupt.timespans[0].start), disrupt.titleSections, disrupt);
+                existingDisruptions = await databaseGetDisruptions();
 
-            sendDiscordNotification('New Disruption', hexToDecimal("#ff0000"), disrupt.timespans[0].situation.label, [
-                {
-                    name: 'ETA',
-                    value: disrupt.expectedDuration.description,
-                    inline: true
-                },
-                {
-                    name: 'Link',
-                    value: `https://www.ns.nl/reisinformatie/actuele-situatie-op-het-spoor/storing?id=${disrupt.id}`,
-                    inline: true
-                }
-            ]);
-        } else {
-            const existingDisruption = existingDisruptions.find(disruption => disruption.nsId === disrupt.id);
-            if (!existingDisruption) return;
-    
-            const disruptionUpdate = await getDisruptionUpdate(disrupt);
-            if (disruptionUpdate.length > 0) return;
-    
-            createDisruptionUpdate(existingDisruption.disruptionId, new Date(disrupt.registrationTime), disrupt);
-            
-            if (disrupt.phase.id == '4') {
-                sendDiscordNotification('Disruption Resolved', hexToDecimal("#00ff00"), disrupt.timespans[0].situation.label, [
+                sendDiscordNotification('New Disruption', hexToDecimal("#ff0000"), disrupt.timespans[0].situation.label, [
                     {
                         name: 'ETA',
                         value: disrupt.expectedDuration.description,
@@ -52,27 +31,52 @@ async function checkDisruptions() {
                         inline: true
                     }
                 ]);
+            } else {
+                const existingDisruption = existingDisruptions.find(disruption => disruption.nsId === disrupt.id);
+                if (!existingDisruption) return;
+
+                const disruptionUpdate = await getDisruptionUpdate(disrupt);
+                if (disruptionUpdate.length > 0) return;
+
+                createDisruptionUpdate(existingDisruption.disruptionId, new Date(disrupt.registrationTime), disrupt);
+
+                if (disrupt.phase.id == '4') {
+                    sendDiscordNotification('Disruption Resolved', hexToDecimal("#00ff00"), disrupt.timespans[0].situation.label, [
+                        {
+                            name: 'ETA',
+                            value: disrupt.expectedDuration.description,
+                            inline: true
+                        },
+                        {
+                            name: 'Link',
+                            value: `https://www.ns.nl/reisinformatie/actuele-situatie-op-het-spoor/storing?id=${disrupt.id}`,
+                            inline: true
+                        }
+                    ]);
+                }
             }
-        }
-    });
+        });
 
-    await existingDisruptions.forEach(async (disruption: Disruption) => {
-        const disruptionUpdates = await getDisruptionUpdatesByDisruptionId(disruption.disruptionId);
-        if (disruptionUpdates.length <= 0) return;
-        const lastUpdate = disruptionUpdates[disruptionUpdates.length - 1]
+        await existingDisruptions.forEach(async (disruption: Disruption) => {
+            const disruptionUpdates = await getDisruptionUpdatesByDisruptionId(disruption.disruptionId);
+            if (disruptionUpdates.length <= 0) return;
+            const lastUpdate = disruptionUpdates[disruptionUpdates.length - 1]
 
-        if (disruption.stations == null) {
-            disruption.stations = await updateStations(disruption, lastUpdate);
-        }
-        
-        if (disruption.stationsGeo == null) {
-            updateStationsGeo(disruption);
-        }
+            if (disruption.stations == null) {
+                disruption.stations = await updateStations(disruption, lastUpdate);
+            }
+            
+            if (disruption.stationsGeo == null) {
+                updateStationsGeo(disruption);
+            }
 
-        if (disruption.timeEnd == null) {
-            updateEnd(disruptions, disruption, lastUpdate);
-        }
-    });
+            if (disruption.timeEnd == null) {
+                updateEnd(disruptions, disruption, lastUpdate);
+            }
+        });
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 if (process.argv.includes('--now')) {
