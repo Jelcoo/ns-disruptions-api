77eb7e2cd9420df3d05b83188bb8089fe671398b
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2024-07-05 21:49:03 +0200

    Allow cors origin

--- src/websocket.ts
+++ src/websocket.ts
@@ -3,7 +3,11 @@ import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 
 const httpServer = createServer();
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.WS_CORS_ORIGIN,
+    }
+});
 
 io.on("connection", (socket: Socket) => {
     console.log("Connected: " + socket.id);
